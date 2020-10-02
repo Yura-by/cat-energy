2ebebb7d6f6c953648730bc56dcd6cc828a952b3
Yura-by <s.older681@gmail.com>
2020-10-02 23:44:48 +0300

    Начнет верстать catalog

--- src/components/page-catalog/page-catalog.tsx
+++ src/components/page-catalog/page-catalog.tsx
@@ -5,9 +5,11 @@ import {AdditionalOffers, MainOffers} from './mock';
 const PageCatalog = () => {
   return (
     <main className="page-catalog">
-      <h2 className="page-catalog__title">
-        Каталог продукции
-      </h2>
+      <div className="page-catalog__top-wrapper">
+        <h2 className="page-catalog__title">
+          Каталог продукции
+        </h2>
+      </div>
 
       <section className="page-catalog__goods goods">
         <h3 className="visually-hidden">Основные виды продукции</h3>
@@ -15,33 +17,41 @@ const PageCatalog = () => {
            {MainOffers.map((it) => {
              return (
               <li className="goods__item" key={it.id}>
-                <h4 className="goods__title">Cat Energy {it.title}</h4>
+                <div className="goods__inner-wrapper">
 
-                <picture>
-                  <source type="image/webp" media="(min-width: 768px)" srcSet={`${it.image.webpDesktop} 1x, ${it.image.webp2xDesktop} 2x`} />
-                  <source type="image/webp" srcSet={`${it.image.webpMobile} 1x, ${it.image.webp2xMobile} 2x`} />
-                  <source media="(min-width: 768px)" srcSet={`${it.image.jpgDesktop} 1x, ${it.image.jpg2xDesktop} 2x`} />
-                  <img className="goods__image" src={it.image.jpgMobile} srcSet={it.image.jpg2xMobile} alt={it.title}
-                    sizes="(min-width: 768px) 195px : 140px"
-                  />
-                </picture>
+                  <picture>
+                    <source type="image/webp" media="(min-width: 768px)" srcSet={`${it.image.webpDesktop} 1x, ${it.image.webp2xDesktop} 2x`} />
+                    <source type="image/webp" srcSet={`${it.image.webpMobile} 1x, ${it.image.webp2xMobile} 2x`} />
+                    <source media="(min-width: 768px)" srcSet={`${it.image.jpgDesktop} 1x, ${it.image.jpg2xDesktop} 2x`} />
+                    <img className="goods__image" src={it.image.jpgMobile} srcSet={it.image.jpg2xMobile} alt={it.title}
+                      sizes="(min-width: 768px) 195px : 140px"
+                    />
+                  </picture>
 
-                <table className="goods__table goods-desc">
-                  <thead>
-                    <tr className="goods-desc__row">
-                      <th className="goods-desc__cell">Объем</th>
-                      <th className="goods-desc__cell">Вкус</th>
-                      <th className="goods-desc__cell">Цена</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr className="goods-desc__row">
-                      <td className="goods-desc__data">{it.data.volume}</td>
-                      <td className="goods-desc__data">{it.data.taste}</td>
-                      <td className="goods-desc__data">{it.data.price}</td>
-                    </tr>
-                  </tbody>
-                </table>
+                  <div className="goods__container">
+                    <h4 className="goods__title">Cat Energy <br />{it.title}</h4>
+
+                    <table className="goods__table goods-desc">
+                      <thead>
+                        <tr className="goods-desc__row">
+                          <th className="goods-desc__cell">Объем</th>
+                          <th className="goods-desc__cell">Вкус</th>
+                          <th className="goods-desc__cell">Цена</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr className="goods-desc__row">
+                          <td className="goods-desc__data">{it.data.volume}</td>
+                          <td className="goods-desc__data">{it.data.taste}</td>
+                          <td className="goods-desc__data">{it.data.price}</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+
+                  <button className="goods-desc__button">Заказать</button>
+
+                </div>
               </li>
              );
            })}
